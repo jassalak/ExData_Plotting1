1158a02501c92b4305f6ee6b95095aefbde9ec82
jassalak <akash.jassal09@gmail.com>
2016-08-21 11:59:09 -0400

    JHU Data Science Specialization
    c4w1

---
+++ plot3.R
@@ -0,0 +1,39 @@
+###############################################################################################################
+#JHU Data Science Specialization --> Course4 Week1
+
+rm(list = ls())
+setwd("C:/Users/akash/Desktop/StatsCourses/JHU_Specialization/Course4/w1")
+###############################################################################################################
+##Loading The Data
+if(!file.exists('data')) dir.create('data')
+fileUrl <- 'https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip'
+download.file(fileUrl, destfile = './data/household_power_consumption.zip')
+unzip('./data/household_power_consumption.zip', exdir = './data')
+
+data_full <- read.csv("household_power_consumption.txt", header = T, sep = ';', 
+                      na.strings = "?", nrows = 2075259, check.names = F, 
+                      stringsAsFactors = F, comment.char = "", quote = '\"')
+
+data_full$Date <- as.Date(data_full$Date, format = "%d/%m/%Y")
+
+dataOne <- subset(data_full, subset = (Date >= "2007-02-01" & Date <= "2007-02-02"))
+
+rm(data_full)
+
+##Converting Dates
+datetime <- paste(as.Date(dataOne$Date), dataOne$Time)
+dataOne$Datetime <- as.POSIXct(datetime)
+
+###############################################################################################################
+##PlotThree
+with(dataOne, {
+  plot(Sub_metering_1 ~ Datetime, type = "l", 
+       ylab = "Global Active Power (kilowatts)", xlab = "")
+  lines(Sub_metering_2 ~ Datetime, col = 'Red')
+  lines(Sub_metering_3 ~ Datetime, col = 'Blue')
+})
+legend("top", col = c("black", "red", "blue"), lty = 1, lwd = 2, 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+###############################################################################################################
+
